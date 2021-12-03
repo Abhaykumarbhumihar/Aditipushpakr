407a8eacd2648b10dacce727dad22da63b25bb97
John <johnf@solidappmaker.com>
2021-12-03 14:47:05 +0530

    updated code

--- app.js
+++ app.js
@@ -9,18 +9,9 @@ var path = require('path');
 app.set('view engine','ejs')
 
  app.use('/images', express.static(path.join(__dirname, 'view/images')))
-//app.use('/', express.static(path.join(__dirname, 'view')))
 
 app.get("/", urlencoded, function (req, res) {
-
-    // inserData(req.query.name, req.query.email, req.query.address)
      res.render('newnav')
  });
-
-
- 
-
-
-
 
 app.listen(port)
