249198dbd3ba5cbf9305ae95a571fd96684b839e
John <johnf@solidappmaker.com>
2021-12-03 14:13:33 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+var urlencoded = bodyparser.urlencoded();
+app.set('view engine', 'ejs')
+var port = process.env.PORT || 3002;
+var path = require('path');
+
+
+
+app.set('view engine','ejs')
+
+
+
+
+
+
+
+ app.use('/images', express.static(path.join(__dirname, 'view/images')))
+//app.use('/', express.static(path.join(__dirname, 'view')))
+
+app.get("/", urlencoded, function (req, res) {
+
+    // inserData(req.query.name, req.query.email, req.query.address)
+     res.render('newnav')
+ });
+
+
+ 
+
+
+
+
+app.listen(port)
